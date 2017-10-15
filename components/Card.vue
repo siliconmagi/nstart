5f581ec35c8bfa76ecc66ec3539d652cf51f830e
siliconmagi <siliconmagi@yandex.com>
2017-10-14 17:43:21 -0700

    components

---
+++ components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container">
+		<!-- Content from the parent gets rendered here. -->
+		<slot></slot>
+	</div>
+</template>
+<style lang="scss" scoped>
+	.container
+	{
+		background-color: black;
+		border-radius: 5px;
+		margin: 0 auto;
+		width: 95%;
+		padding: 10px;
+		box-shadow: 0px 1px 4px 2px rgba(0,0,0,0.85);
+	}
+	@media (min-width: 1055px) {
+		.container {
+			width: 1000px;
+		}
+	}
+</style>
